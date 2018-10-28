5d2731575e03fe906de8078387156d87516c179b
sergio <seralra96@gmail.com>
2018-10-28 14:07:37 -0500

    Intro solo sale la primera vez

--- app/src/main/java/com/sergio/android/catsigar/SplashScreen.java
+++ app/src/main/java/com/sergio/android/catsigar/SplashScreen.java
@@ -2,6 +2,7 @@ package com.sergio.android.catsigar;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,26 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
+        SharedPreferences pref = getSharedPreferences("prefs", MODE_PRIVATE);
+        boolean firsStart = pref.getBoolean("firstStart", true);
+
+        if (firsStart) {
+            starIntro();
+        } else{
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this, Mod_geo.class);
+                startActivity(intent);
+                finish();
+            }
+        },700);}
+
+    }
+
+    protected void starIntro(){
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -23,6 +44,10 @@ public class SplashScreen extends Activity {
             }
         },700);
 
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("firstStart", false);
+        editor.apply();
     }
 
 
